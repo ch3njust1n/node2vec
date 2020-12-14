491c2d09457b326c91675500892e65bf5ff96fc6
Justin Chen <ch3njus@gmail.com>
2020-12-13 19:20:50 -0500

    Converting repo to be installable via pip
    Updated .gitignore to hide test graph data, output emb folder and main.py. Also move `Graph()` to `__main__.py` and created `version.py` and `__init__.py` so can eventually also be installed via a `setup.py`.

---
+++ node2vec/__init__.py
@@ -0,0 +1,10 @@
+'''
+Node2Vec
+'''
+__title__ = "node2vec"
+__author__ = "Aditya Grover"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2016 Aditya Grover"
+
+from node2vec.version import __version__
+from node2vec.__main__ import Graph
